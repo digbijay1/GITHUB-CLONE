124f6c127af0509a8bc54c915ca1dbf1d30f26e6
digbijay1 <digbijay.das@suiit.ac.in>
2020-12-16 01:46:06 +0530

    final commit

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ async function loadUser(a) {
 
         const response = await fetch(url);
         var data = await response.json();
+        console.log(data)
         var div = document.createElement("div")
         div.setAttribute("class", "digu")
         div.setAttribute("id", "kajal")
@@ -30,6 +31,44 @@ async function loadUser(a) {
         for (var i = 0; i < data.length; i++) {
             var li = document.createElement("li")
             li.innerHTML = data[i].name;
+            li.setAttribute("id","singh")
+           
+            var ij = data[i].name
+            await loadUser2(a,ij)
+            async function loadUser2(a,ij) {
+                try {
+                    const url = `https://api.github.com/repos/${a}/${ij}/git/trees/master?recursive=1`
+                    const response1 = await fetch(url);
+                    var data1 = await response1.json();
+                  //console.log(data1)
+                  var result = Object.keys(data1).map((key) => [Number(key), data1[key]]);
+                  //console.log(result.length)
+    
+                  //console.log(typeof(data1))
+                    
+                  //console.log(data1.tree[i.path])
+                  //console.log(result[2][1])
+                  
+    
+                  for(var i=0;i<result[2][1].length;i++){
+                    //  document.getElementById("singh").innerHTML+=`<li></li>`
+        //var li1=document.createElement("li")
+                   // ul.appendChild(li1)
+                   var ul1=document.createElement("ul")
+                   li.appendChild(ul1)
+                   ul1.setAttribute("class","bablu")
+                   var li1=document.createElement("li")
+                   ul1.appendChild(li1)
+                   li1.innerHTML=result[2][1][i].path
+                      console.log(result[2][1][i].path)
+                  }
+                    
+                 // console.log(typeof(data1.tree[0]))
+                }
+                catch (err) {
+                    console.log("err1")
+                }
+            }
             ul.appendChild(li)
             div.appendChild(ul)
             const newsBody = document.getElementById('newsBody');
@@ -38,12 +77,12 @@ async function loadUser(a) {
 
 
 
-            
-    var ij = data[i].name
-           // console.log(ij);
+         // console.log(data)  
+    
+           //console.log(ij);
            //bro here i am facing the issue i have just uncomment the things
-           
-          await loadUser2(a,ij)
+          
+        
             
            
           
@@ -57,18 +96,7 @@ async function loadUser(a) {
     }
    
 }
- async function loadUser2(a,ij) {
-                try {
-                    const url = `https://api.github.com/repos/${a}/${ij}/git/trees/master?recursive=1`
-                    const response = await fetch(url);
-                    var data = await response.json();
-                  console.log(data.tree[3].path)
-                 // console.log(typeof(data1.tree[0]))
-                }
-                catch (err) {
-                    console.log("err1")
-                }
-            }
+
 
 
 
